3a0a7c8028d62999a838d66d11d109177ebaa6d2
Irina Park <park_irina@naver.com>
2021-02-07 02:43:14 +0900

    Initial commit

---
+++ src/main/java/koschei/models/Rabbit4.java
@@ -0,0 +1,15 @@
+package koschei.models;
+
+public class Rabbit4 {
+
+    private Duck5 duck;
+
+    public Rabbit4 (Duck5 duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public String toString() {
+        return ", в зайце утка " + duck.toString();
+    }
+}
